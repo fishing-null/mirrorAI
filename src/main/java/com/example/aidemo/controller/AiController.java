c61a309f8992d66d306871c7f08ae3bbd94d2271
MatouSakura <1134979484@qq.com>
2024-06-16 22:40:08 +0800

    update

--- src/main/java/com/example/aidemo/controller/AiController.java
+++ src/main/java/com/example/aidemo/controller/AiController.java
@@ -1,6 +1,7 @@
 package com.example.aidemo.controller;
 
 import com.example.aidemo.service.Completion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,11 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class AiController {
-    private final Completion completion;
+    @Autowired
+    private  Completion completion;
 
-    public AiController(Completion completion){
-        this.completion = completion;
-    }
     @GetMapping("/chat")
     public String chat(@RequestParam(value = "message",defaultValue = "Hi") String message){
 
